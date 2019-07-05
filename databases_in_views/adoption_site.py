7fff858280617857a7e5cfd9eb8fba4ff93e72dd
Marcin Sokolowski <sokolowski.marcin@me.com>
2019-07-05 15:15:19 +0200

    working

--- databases_in_views/adoption_site.py
+++ databases_in_views/adoption_site.py
@@ -1,5 +1,5 @@
 import os
-# from forms import AddForm, DelForm
+from forms import AddForm, DelForm
 from flask import Flask, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -9,8 +9,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret key'
 
 # SQL DATABASE SECTION#
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(BASEDIR, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -32,3 +32,49 @@ class Puppy(db.Model):
         
     def __repr__(self):
         return f'Puppy name: {self.name}'
+    
+
+# VIEW FUNCTION #
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_pup():
+    
+    form = AddForm()
+    
+    if form.validate_on_submit():
+        name = form.name.data
+        new_pup = Puppy(name)
+        db.session.add(new_pup)
+        db.session.commit()
+
+        return redirect(url_for('list_pup'))
+
+    return render_template('add.html', form=form)
+
+
+@app.route('/list')
+def list_pup():
+    
+    puppies = Puppy.query.all()
+    return render_template('list.html', puppies=puppies)
+
+
+@app.route('/del', methods=['GET','POST'])
+def del_pup():
+    
+    form = DelForm()
+    if form.validate_on_submit():
+        id = form.id.data
+        pup = Puppy.query.get(id)
+        db.session.delete(pup)
+        db.session.commit()
+        return redirect(url_for('list_pup'))
+    return render_template('delete.html', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
