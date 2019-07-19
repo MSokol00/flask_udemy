557a32b22adcfb4efc9b0cf3e56609063a43707b
Marcin <sokolowski.marcin@me.com>
2019-07-19 15:07:38 +0200

    course done

--- REST/simpleapi.py
+++ REST/simpleapi.py
@@ -1,19 +1,74 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Resource, Api
+from secure_check import authenticate, identity
+from flask_jwt import JWT, jwt_required
+from flask_migrate import Migrate
+import os
 
-app = Flask(__name__)
+APP = Flask(__name__)
+APP.config['SECRET_KEY'] = 'mysecretkey'
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+APP.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(BASEDIR, 'data.sqlite')
+APP.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+DB = SQLAlchemy(APP)
+Migrate(APP, DB)
 
-api = Api(app)
+API = Api(APP)
+JWT = JWT(APP, authenticate, identity)
 
 
-class HelloWorld(Resource):
+##########
+class Puppy(DB.Model):
+    name = DB.Column(DB.String(80), primary_key=True)
     
+    def __init__(self, name):
+        self.name = name
+        
+    def json(self):
+        return {'name': self.name}
+##############
+
+class PuppyNames(Resource):
+    """PuppyClass"""
+
+    def get(self, name):
+        """get method"""
+        pup = Puppy.query.filter_by(name=name).first()
+        if pup:
+            return pup.json()
+        return {'name': None}, 404
+
+    def post(self, name):
+        """post method"""
+        pup = Puppy(name=name)
+        DB.session.add(pup)
+        DB.session.commit()
+        return pup.json()
+
+    def delete(self, name):
+        """delete method"""
+        pup = Puppy.query.filter_by(name=name).first()
+        if pup:
+            DB.session.delete(pup)
+            DB.session.commit()
+            return {'note': 'delete success'}
+        return {'name': None}, 404
+
+
+class AllNames(Resource):
+    """AllNames"""
+
+    #@jwt_required()
     def get(self):
-        return {'hello':'world'}
+        """get method"""
+        puppies = Puppy.query.all()
+        
+        return [pup.json() for pup in puppies]
 
-
-api.add_resource(HelloWorld, '/')
+API.add_resource(PuppyNames, '/puppy/<string:name>')
+API.add_resource(AllNames, '/puppies')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    APP.run(debug=True)
